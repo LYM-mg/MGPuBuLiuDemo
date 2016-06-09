d4ec3c0e8176104475d4d4950f03607faa556309
ming <mg13750525922@sina.com>
2016-06-10 00:50:20 +0800

    解决界面不现实BUG

---
+++ MGPuBuLiuDemo/MGPuBuLiuDemo/Lib/Layout/MGWaterflowLayout.h
@@ -0,0 +1,34 @@
+//
+//  MGWaterflowLayout.h
+//  MGPuBuLiuDemo
+//
+//  Created by ming on 16/6/9.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@class MGWaterflowLayout;
+
+@protocol MGWaterflowLayoutDelegate <NSObject>
+@required
+- (CGFloat)waterflowLayout:(MGWaterflowLayout *)waterflowLayout heightForItemAtIndex:(NSIndexPath *)indexPath itemWidth:(CGFloat)itemWidth;
+
+@optional
+- (CGFloat)columnCountInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout;
+- (CGFloat)columnMarginInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout;
+- (CGFloat)rowMarginInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout;
+- (UIEdgeInsets)edgeInsetsInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout;
+
+@end
+
+
+
+
+@interface MGWaterflowLayout : UICollectionViewFlowLayout
+
+/** 代理 */
+@property (nonatomic, weak) id<MGWaterflowLayoutDelegate> delegate;
+
+@end
--- MGPuBuLiuDemo/MGPuBuLiuDemo/ViewController.m
+++ MGPuBuLiuDemo/MGPuBuLiuDemo/ViewController.m
@@ -15,7 +15,7 @@
 
 static NSString *const ShopCellIdentifier = @"ShopCellIdentifier";
 
-@interface ViewController ()<UICollectionViewDataSource>
+@interface ViewController ()<UICollectionViewDataSource,MGWaterflowLayoutDelegate>
 /** 所有的商品数据 */
 @property (nonatomic, strong) NSMutableArray *shops;
 
@@ -36,27 +36,70 @@ static NSString *const ShopCellIdentifier = @"ShopCellIdentifier";
     [super viewDidLoad];
 
     [self setUpLayout];
+    
+    [self setupRefresh];
+    
+    // 第一次刷新手动调用
+    [self.collectionView.header beginRefreshing];
 }
 
+/// 初始化collectionView
 - (void)setUpLayout{
     // 创建布局
     MGWaterflowLayout *flowLayout = [[MGWaterflowLayout alloc] init];
+    flowLayout.delegate = self;
     
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flowLayout];
-    collectionView.dataSource = self;
+    // 创建CollectionView
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
     collectionView.backgroundColor = [UIColor whiteColor];
-    collectionView.frame = self.view.frame;
+    collectionView.dataSource = self;
+    [self.view addSubview:collectionView];
+
+    
+    // 注册
     [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([MGShopCell class]) bundle:nil] forCellWithReuseIdentifier:ShopCellIdentifier];
     [self.view addSubview:collectionView];
     self.collectionView = collectionView;
 }
 
+/// 刷新加载数据
+- (void)setupRefresh{
+    // 上拉刷新
+    self.collectionView.header = [MJRefreshGifHeader headerWithRefreshingBlock:^{
+       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSArray *shops = [MGShopModel objectArrayWithFilename:@"1.plist"];
+            [self.shops removeAllObjects];
+            [self.shops addObjectsFromArray:shops];
+            
+            // 刷新数据
+            [self.collectionView reloadData];
+            
+            [self.collectionView.header endRefreshing];
+        });
+    }];
+     
+    // 下拉刷新
+    self.collectionView.footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSArray *shops = [MGShopModel objectArrayWithFilename:@"1.plist"];
+            [self.shops addObjectsFromArray:shops];
+            
+            // 刷新数据
+            [self.collectionView reloadData];
+            
+            [self.collectionView.footer endRefreshing];
+        });
+    }];
+    
+    self.collectionView.footer.hidden = YES;
+}
+
 #pragma mark - <UICollectionViewDataSource>
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 39;
-//    self.collectionView.footer.hidden = self.shops.count == 0;
-//    return self.shops.count;
+//    return 39;
+    self.collectionView.footer.hidden = self.shops.count == 0;
+    return self.shops.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
@@ -65,6 +108,30 @@ static NSString *const ShopCellIdentifier = @"ShopCellIdentifier";
      cell.shop = self.shops[indexPath.item];
     
     return cell;
+}
+
+#pragma mark - <MGWaterflowLayoutDelegate>
+- (CGFloat)waterflowLayout:(MGWaterflowLayout *)waterflowLayout heightForItemAtIndex:(NSIndexPath *)indexPath itemWidth:(CGFloat)itemWidth
+{
+    MGShopModel *shop = self.shops[indexPath.item];
+    
+    return itemWidth * shop.h / shop.w;
+}
+
+- (CGFloat)rowMarginInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout
+{
+    return 20;
+}
+
+- (CGFloat)columnCountInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout
+{
+    if (self.shops.count <= 50) return 2;
+    return 3;
+}
+
+- (UIEdgeInsets)edgeInsetsInWaterflowLayout:(MGWaterflowLayout *)waterflowLayout
+{
+    return UIEdgeInsetsMake(10, 20, 30, 100);
 }
 
 @end
